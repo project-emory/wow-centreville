96113fde908b70c5a3e2752c1fdedd60041123b4
andrewtlu <andrew.lu@emory.edu>
2024-11-05 14:30:10 -0500

    Create initial models

--- backend/wow_backend/api/models.py
+++ backend/wow_backend/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """Model for site users."""
+    pass
+
+
+class Order(models.Model):
+    """Model for user orders."""
+    pass
+
+
+class OrderItem(models.Model):
+    """Intermediary model for items in a user's orders."""
+    pass
+
+
+class MenuItem(models.Model):
+    """Model for menu items."""
+    pass
