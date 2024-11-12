56f0feac421960c03fd5d8720d55110e13b835e8
TAEEUN KIM <taeeunk1208@gmail.com>
2024-11-12 18:04:58 -0500

    deploy workflow

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
+    basePath: "/frontend",
+    output: "export",
+    reactStrictMode: true,
 };
 
 export default nextConfig;
