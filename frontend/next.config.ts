a4bccd39c54b09b3c05f2afab3961b7aa4b5dd5c
TAEEUN KIM <taeeunk1208@gmail.com>
2024-11-12 21:29:44 -0500

    remove basePath for custom domain

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    basePath: "/wow-centreville",
     output: "export",
     reactStrictMode: true,
     images: {
