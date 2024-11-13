acbd8074535eb67a96960b88a8d5a75766d16701
TAEEUN KIM <taeeunk1208@gmail.com>
2024-11-13 15:47:26 -0500

    favicon update

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -24,6 +24,16 @@ const RootLayout = ({
 }>) => {
     return (
         <html lang="en">
+            <head>
+            <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+            <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+            <link rel="shortcut icon" href="/favicon/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+            <meta name="apple-mobile-web-app-title" content="WOW" />
+            <link rel="manifest" href="/favicon/site.webmanifest" />
+            </head>
             <body
                 className={`${antonio.variable} ' flex min-h-screen flex-col bg-[#F3EDE1] font-antonio text-black antialiased`}
             >
