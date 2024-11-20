4e9feaab9d5f5e3a66ecef07db2fc3df25095791
TAEEUN KIM <taeeunk1208@gmail.com>
2024-11-19 20:25:58 -0500

    dropdown-menu setup, organize ui components

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,3 +1,2 @@
 export { default as Header } from "./Header";
-export { default as Button } from "./Button";
 export { default as Footer } from "./Footer";
---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,18 @@
+export { default as Button } from "./Button";
+export { 
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuCheckboxItem,
+    DropdownMenuRadioItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuGroup,
+    DropdownMenuPortal,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuRadioGroup,
+} from "./dropdown-menu";
