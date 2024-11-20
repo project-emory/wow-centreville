a69ccf7fd952feea2c55f7ddf9ac16a56859118b
TAEEUN KIM <taeeunk1208@gmail.com>
2024-11-19 23:25:37 -0500

    about page language setup

--- frontend/src/libs/i18n.ts
+++ frontend/src/libs/i18n.ts
@@ -4,15 +4,19 @@ import koHeader from "@/i18n/ko/header.json";
 import enHeader from "@/i18n/en/header.json";
 import koHome from "@/i18n/ko/home.json";
 import enHome from "@/i18n/en/home.json";
+import koAbout from "@/i18n/ko/about.json";
+import enAbout from "@/i18n/en/about.json";
 
 const resources = {
     ko: {
         header: koHeader,
         home: koHome,
+        about: koAbout,
     },
     en: {
         header: enHeader,
         home: enHome,
+        about: enAbout,
     },
 };
 
