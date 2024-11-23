19031b77746cb73b52363d2688c62beb32b49820
TAEEUN KIM <taeeunk1208@gmail.com>
2024-11-23 17:05:20 -0500

    home page responsive

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,34 +9,34 @@ export default function Home() {
     const { t } = useTranslation("home");
     return (
         <div className="min-h-screen justify-items-center py-[4.75rem]">
-            <main className="mb-20 flex w-[60vw] flex-col items-center gap-8 overflow-x-hidden pt-8 sm:items-start">
-                <div className="flex w-full items-center justify-between">
-                    <div className="text-[5em] font-bold leading-[7.5rem] text-wow-red">
-                        <div className="text-[7rem]">WOW</div>
-                        <div>{t("WOW.1")}</div>
+            <main className="flex w-[60vw] flex-col items-center gap-8 pt-8 sm:items-start">
+                <div className="md:flex w-full items-center justify-between">
+                    <div className="md:block flex flex-col items-center font-bold md:leading-[7.5rem] leading-[15vw] text-wow-red">
+                        <div className="md:text-[7rem] sm:text-[15vw] text-[14vw]">WOW</div>
+                        <div className="md:text-[5rem] sm:text-[12vw] text-[10vw]">{t("WOW.1")}</div>
                     </div>
                     <div>
                         <Image
                             src={main1}
                             alt="엄마밥상 placeholder image"
-                            className="h-[35vh] w-[32vw] rounded-md object-cover shadow-md"
+                            className="md:h-[35vh] md:w-[32vw] w-full max-md:max-h-[25vh] rounded-md object-cover shadow-md"
                         />
                     </div>
                 </div>
-                <div className="mt-4 flex w-full items-center justify-between">
-                    <div>
+                <div className="md:flex md:flex-row flex flex-col mt-4 w-full items-center justify-between">
+                    <div className="md:order-1 order-2">
                         <Image
                             src={main2}
                             alt="엄마밥상 placeholder image"
-                            className="h-[35vh] w-[30vw] rounded-md object-cover shadow-md"
+                            className="md:h-[35vh] md:w-[32vw] w-full max-md:max-h-[25vh] rounded-md object-cover shadow-md"
                         />
                     </div>
-                    <div className="text-right text-[5em] font-bold leading-[7.5rem] text-wow-red">
-                        <div className="text-[7rem]">WOW</div>
-                        <div>{t("WOW.2")}</div>
+                    <div className="md:order-2 order-1 md:block flex flex-col items-center text-right text-[5em] font-bold md:leading-[7.5rem] leading-[15vw] text-wow-red">
+                        <div className="md:text-[7rem] sm:text-[15vw] text-[14vw]">WOW</div>
+                        <div className="md:text-[5rem] sm:text-[12vw] text-[10vw]">{t("WOW.2")}</div>
                     </div>
                 </div>
-                <div className="absolute bottom-[1rem] left-0 flex w-full animate-bounce flex-col items-center text-[3rem] font-bold text-wow-red">
+                <div className="flex w-full -mt-8 animate-bounce flex-col items-center text-[3rem] font-bold text-wow-red">
                     <div>&#8964;</div>
                 </div>
             </main>
