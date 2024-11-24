a2479b8640602857b1e74be9ab1527ba091d0960
TAEEUN KIM <taeeunk1208@gmail.com>
2024-11-24 02:09:53 -0500

    about korean setting responsiveness fixed

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/src/components";
 import { useTranslation } from "react-i18next";
 
 const About = () => {
-    const { t } = useTranslation("about");
+    const { t, i18n } = useTranslation("about");
     return (
         <div className="mt-[2.5rem] flex min-h-screen w-full justify-center overflow-x-hidden py-[4.75rem]">
             <div className="flex w-[55vw] flex-col items-center justify-center gap-8">
@@ -14,7 +14,7 @@ const About = () => {
                     alt="White WOW Logo"
                     className="h-[10rem] w-[10.5rem] object-cover object-left"
                 />
-                <div className="text-[2.5rem] font-bold text-wow-red">{t("title")}</div>
+                <div className={`${i18n.language === 'ko' ? "text-[2rem]" : "text-[2.5rem]"} md:text-[3rem] font-bold text-wow-red`}>{t("title")}</div>
                 <p className="text-center leading-8">
                     {t("description.1")} <br />
                     {t("description.2")}
