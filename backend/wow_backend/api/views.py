7734bf013286996030e28cfe13ccf41edd2a2d7b
andrewtlu <andrew.lu@emory.edu>
2024-12-03 16:33:01 -0500

    Add initial example users viewset and url

--- backend/wow_backend/api/views.py
+++ backend/wow_backend/api/views.py
@@ -1,3 +1,23 @@
-# from django.shortcuts import render
+from rest_framework.mixins import (
+    CreateModelMixin,
+    ListModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+)
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializer
+
+
+class UserViewSet(
+    GenericViewSet,  # generic view functionality
+    CreateModelMixin,  # handles POST
+    RetrieveModelMixin,  # handles GETs for 1
+    UpdateModelMixin,  # handles PUTs and PATCHes
+    ListModelMixin,
+):  # handles GETs for many
+    """View set for the `User` model."""
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
--- backend/wow_backend/urls.py
+++ backend/wow_backend/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from api.views import UserViewSet
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+router = DefaultRouter()
+router.register(r"users", UserViewSet)
+
+urlpatterns = router.urls
