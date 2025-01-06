9371699405fe86eec8260e13f3f76b6be8fb6289
Ainsley Freedman <ainsley.freedman@emory.edu>
2025-01-06 14:39:54 -0500

    Create Sign In and Sign Up Pages With Proper Icons

--- frontend/src/app/signin/page.tsx
+++ frontend/src/app/signin/page.tsx
@@ -1,63 +1,67 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
-import { useRouter, usePathname } from "next/navigation";
+import React, { useState } from "react";
+import Image from "next/image";
+import { eye, eyeSlash } from "@/public";
 
-const SignIn = () => {
-    const [isPasswordVisible, setIsPasswordVisible] = useState(false); // State to toggle password visibility
-    const [isIconVisible, setIsIconVisible] = useState(false); // State to control eye icon visibility after the rest of the elements
-
-    // Ensure icon is only rendered after the component has fully loaded
-    useEffect(() => {
-        setIsIconVisible(true); // Make eye icon visible after the component mounts
-    }, []);
+const SignUp = () => {
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
     const togglePasswordVisibility = () => {
         setIsPasswordVisible(!isPasswordVisible); // Toggle visibility
-    };
-
-    const router = useRouter();
-    const handleSignUpClick = () => {
-        router.push("/signup");
     };
 
     return (
         <div className="mt-[2.5rem] flex min-h-screen w-full justify-center overflow-x-hidden py-[4.75rem]">
             <div className="flex w-[55vw] flex-col items-center gap-8">
                 <br />
-                <div className="text-[3.5rem] font-semibold text-wow-red">Sign In</div>
+                <div className="text-[3.5rem] font-semibold text-wow-red">Sign Up</div>
                 <input
                     type="text"
-                    className="h-10 w-80 bg-[#D9D9D9] px-2 placeholder:text-xl text-xl text-[#C61C00] placeholder:text-[#C61C00] placeholder:opacity-35"
+                    className="h-10 w-80 bg-[#D9D9D9] px-2 text-xl text-[#C61C00] placeholder:text-xl placeholder:text-[#C61C00] placeholder:opacity-35"
                     placeholder="Phone Number"
+                    required
+                />
+                <input
+                    type="text"
+                    className="h-10 w-80 bg-[#D9D9D9] px-2 text-xl text-[#C61C00] placeholder:text-xl placeholder:text-[#C61C00] placeholder:opacity-35"
+                    placeholder="First Name"
+                    required
+                />
+                <input
+                    type="text"
+                    className="h-10 w-80 bg-[#D9D9D9] px-2 text-xl text-[#C61C00] placeholder:text-xl placeholder:text-[#C61C00] placeholder:opacity-35"
+                    placeholder="Last Name"
                     required
                 />
                 <div className="relative w-80">
                     <input
-                        type={isPasswordVisible ? "text" : "password"} // Toggle password visibility
-                        className="h-10 w-full bg-[#D9D9D9] px-2 text-[#C61C00] placeholder-text-xl text-xl placeholder:text-[#C61C00] placeholder:opacity-35"
+                        type={isPasswordVisible ? "text" : "password"}
+                        className="placeholder-text-xl h-10 w-full bg-[#D9D9D9] px-2 text-xl text-[#C61C00] placeholder:text-[#C61C00] placeholder:opacity-35"
                         placeholder="Password"
                         required
                     />
-                    {/* Eye icon to toggle password visibility */}
-                    {isIconVisible && (
-                        <FontAwesomeIcon
-                            icon={isPasswordVisible ? faEye : faEyeSlash}
-                            onClick={togglePasswordVisibility}
-                            className="absolute right-2 top-1/2 -translate-y-1/2 transform cursor-pointer text-md text-[#C61C00]"
+                    <div
+                        className={`absolute right-2 top-1/2 -translate-y-1/2 transform cursor-pointer ${
+                            !isPasswordVisible ? "translate-x-[5%]" : ""
+                        }`} // Apply translation only when password is hidden
+                        onClick={togglePasswordVisibility}
+                    >
+                        <Image
+                            src={isPasswordVisible ? eye : eyeSlash} // Toggle between the icons
+                            alt="eye-icon"
                         />
-                    )}
+                    </div>
                 </div>
-                <div className="relative left-[102px] -mt-5 cursor-pointer text-[1.15rem] text-[#C61C00] hover:underline">
-                    Forgot Password?
-                </div>
-                <button className="bg-wow-red text-wow-cream w-80 h-12 text-[1.5rem] rounded-[5px] hover:bg-[#af1800] transition-bg duration-300">Login</button>
-                <div className="text-[1.15rem] text-[#C61C00] -mb-7">New User?</div>
-                <button className="bg-[#D9D9D9] text-wow-red w-80 h-12 text-[1.5rem] rounded-[5px] hover:bg-[#C4C4C4] transition-bg duration-300" onClick={handleSignUpClick}>Create Account</button>
+                <button className="transition-bg h-12 w-80 rounded-[5px] bg-wow-red text-[1.5rem] text-wow-cream duration-300 hover:bg-[#af1800]">
+                    Create Account
+                </button>
+                <div className="-mb-7 text-[1.15rem] text-[#C61C00]">Already have an account?</div>
+                <button className="transition-bg h-12 w-80 rounded-[5px] bg-[#D9D9D9] text-[1.5rem] text-wow-red duration-300 hover:bg-[#C4C4C4]">
+                    Sign In
+                </button>
             </div>
         </div>
     );
 };
 
-export default SignIn;
+export default SignUp;
--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
-import { useRouter, usePathname } from "next/navigation";
+import Image from "next/image";
+import { eye, eyeSlash } from "@/public";
+import { useRouter } from "next/navigation";
 
-const signUp = () => {
+const SignUp = () => {
     const [isPasswordVisible, setIsPasswordVisible] = useState(false); // State to toggle password visibility
     const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState(false); // State for confirm password
     const [isIconVisible, setIsIconVisible] = useState(false); // State to control eye icon visibility after the rest of the elements
@@ -34,58 +34,79 @@ const signUp = () => {
                 <div className="text-[3.5rem] font-semibold text-wow-red">Sign Up</div>
                 <input
                     type="text"
-                    className="h-10 w-80 bg-[#D9D9D9] px-2 placeholder:text-xl text-xl text-[#C61C00] placeholder:text-[#C61C00] placeholder:opacity-35"
+                    className="h-10 w-80 bg-[#D9D9D9] px-2 text-xl text-[#C61C00] placeholder:text-xl placeholder:text-[#C61C00] placeholder:opacity-35"
                     placeholder="Phone Number"
                     required
                 />
                 <input
                     type="text"
-                    className="h-10 w-80 bg-[#D9D9D9] px-2 placeholder:text-xl text-xl text-[#C61C00] placeholder:text-[#C61C00] placeholder:opacity-35"
+                    className="h-10 w-80 bg-[#D9D9D9] px-2 text-xl text-[#C61C00] placeholder:text-xl placeholder:text-[#C61C00] placeholder:opacity-35"
                     placeholder="First Name"
                     required
                 />
                 <input
                     type="text"
-                    className="h-10 w-80 bg-[#D9D9D9] px-2 placeholder:text-xl text-xl text-[#C61C00] placeholder:text-[#C61C00] placeholder:opacity-35"
+                    className="h-10 w-80 bg-[#D9D9D9] px-2 text-xl text-[#C61C00] placeholder:text-xl placeholder:text-[#C61C00] placeholder:opacity-35"
                     placeholder="Last Name"
                     required
                 />
                 <div className="relative w-80">
                     <input
                         type={isPasswordVisible ? "text" : "password"} // Toggle password visibility
-                        className="h-10 w-full bg-[#D9D9D9] px-2 text-[#C61C00] placeholder-text-xl text-xl placeholder:text-[#C61C00] placeholder:opacity-35"
+                        className="placeholder-text-xl h-10 w-full bg-[#D9D9D9] px-2 text-xl text-[#C61C00] placeholder:text-[#C61C00] placeholder:opacity-35"
                         placeholder="Password"
                         required
                     />
                     {isIconVisible && (
-                        <FontAwesomeIcon
-                            icon={isPasswordVisible ? faEyeSlash : faEye}
-                            className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer text-[#C61C00]"
-                            onClick={togglePasswordVisibility}
-                        />
+                        <div
+                            className={`absolute right-2 top-1/2 -translate-y-1/2 transform cursor-pointer ${
+                                !isPasswordVisible ? "translate-x-[5%]" : ""
+                            }`} // Apply translation only when password is hidden
+                            onClick={togglePasswordVisibility} // Toggle the password visibility
+                        >
+                            <Image
+                                src={isPasswordVisible ? eye : eyeSlash} // Toggle between the icons
+                                alt="eye-icon"
+                            />
+                        </div>
                     )}
                 </div>
+
                 <div className="relative w-80">
                     <input
                         type={isConfirmPasswordVisible ? "text" : "password"} // Toggle confirm password visibility
-                        className="h-10 w-full bg-[#D9D9D9] px-2 text-[#C61C00] placeholder-text-xl text-xl placeholder:text-[#C61C00] placeholder:opacity-35"
+                        className="placeholder-text-xl h-10 w-full bg-[#D9D9D9] px-2 text-xl text-[#C61C00] placeholder:text-[#C61C00] placeholder:opacity-35"
                         placeholder="Confirm Password"
                         required
                     />
                     {isIconVisible && (
-                        <FontAwesomeIcon
-                            icon={isConfirmPasswordVisible ? faEyeSlash : faEye}
-                            className="absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer text-[#C61C00]"
-                            onClick={toggleConfirmPasswordVisibility}
-                        />
+                        <div
+                            className={`absolute right-2 top-1/2 -translate-y-1/2 transform cursor-pointer ${
+                                !isConfirmPasswordVisible ? "translate-x-[5%]" : ""
+                            }`} // Apply translation only when confirm password is hidden
+                            onClick={toggleConfirmPasswordVisibility} // Toggle the confirm password visibility
+                        >
+                            <Image
+                                src={isConfirmPasswordVisible ? eye : eyeSlash} // Toggle between the icons
+                                alt="eye-icon"
+                            />
+                        </div>
                     )}
                 </div>
-                <button className="bg-wow-red text-wow-cream w-80 h-12 text-[1.5rem] rounded-[5px] hover:bg-[#af1800] transition-bg duration-300">Create Account</button>
-                <div className="text-[1.15rem] text-[#C61C00] -mb-7">Already have an account?</div>
-                <button className="bg-[#D9D9D9] text-wow-red w-80 h-12 text-[1.5rem] rounded-[5px] hover:bg-[#C4C4C4] transition-bg duration-300" onClick={handleSignInClick}>Sign In</button>
+
+                <button className="transition-bg h-12 w-80 rounded-[5px] bg-wow-red text-[1.5rem] text-wow-cream duration-300 hover:bg-[#af1800]">
+                    Create Account
+                </button>
+                <div className="-mb-7 text-[1.15rem] text-[#C61C00]">Already have an account?</div>
+                <button
+                    className="transition-bg h-12 w-80 rounded-[5px] bg-[#D9D9D9] text-[1.5rem] text-wow-red duration-300 hover:bg-[#C4C4C4]"
+                    onClick={handleSignInClick}
+                >
+                    Sign In
+                </button>
             </div>
         </div>
     );
 };
 
-export default signUp;
+export default SignUp;
