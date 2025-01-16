c53c688195afb814d2d82d0a78991bb028402b5d
andrew-Linsanity <andrew.preferredandy@gmail.com>
2025-01-15 23:51:37 -0500

    Remove unused import

--- backend/wow_backend/api/views.py
+++ backend/wow_backend/api/views.py
@@ -6,7 +6,6 @@ from api.serializers import MenuItemSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status 
-from rest_framework.decorators import api_view
 
 class MenuItemView(APIView): 
     def get(self, request, **kwargs):
