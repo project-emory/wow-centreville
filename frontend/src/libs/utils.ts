1daf7df244d4e80fb3cc9fd60211b5462cfe8c55
TAEEUN KIM <taeeunk1208@gmail.com>
2025-02-02 22:56:46 -0500

    format

--- frontend/src/libs/utils.ts
+++ frontend/src/libs/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// merge multiple classnames conditionally 
+// merge multiple classnames conditionally
 // clsx is a utility function that merges multiple classnames into a single string
 // twMerge is a utility function that merges multiple tailwind classes into a single string
 export function cn(...inputs: ClassValue[]) {
